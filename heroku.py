37ecd902352e60a8b7b5feea7eedece17a62f0f2
guemriaida <61495362+guemriaida@users.noreply.github.com>
2021-11-14 21:09:50 +0100

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -1,6 +1,12 @@
-user_input = str(input("Enter a Phrase: "))
-text = user_input.split()
-a = " "
-for i in text:
-    a = a+str(i[0]).upper()
-print(a)
+import os
+from selenium import webdriver
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--disable-dev-sh-usage")
+
+driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
+
+driver.get("http://youtube.com")
+print(driver.page_source)
