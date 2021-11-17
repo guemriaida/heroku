421450cfe07a3a054680553c3e9a38e904c05fcc
guemriaida <61495362+guemriaida@users.noreply.github.com>
2021-11-17 12:05:31 +0100

    Update heroku.py

--- heroku.py
+++ heroku.py
@@ -15,20 +15,22 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
-
+def puis(x, y):
+    return x ** y
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Puissance")
 
 while True:
     # take input from the user
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
     # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -43,6 +45,8 @@ while True:
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
+        elif choice == '5':
+            print(num1, "**", num2, "=", puis(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
